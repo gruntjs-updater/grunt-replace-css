37d79655aaa8664038ed508c3e5855ce07334318
Pai Peng <pai.peng@apparentmedia.de>
2015-06-11 16:18:40 +0200

    init project

---
+++ Gruntfile.js
@@ -0,0 +1,84 @@
+/*
+ * grunt-replace-css
+ * https://github.com/paipeng/grunt-replace-css
+ *
+ * Copyright (c) 2015 Pai Peng
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    replace_css: {
+      default_options: {
+        options: {
+        },
+        files: {
+          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      },
+      custom_options: {
+        options: {
+          separator: ': ',
+          punctuation: ' !!!'
+        },
+        files: {
+          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+        }
+      },
+        html_options: {
+            options: {
+                prefix: '',
+                offset: 1,
+                remove_blank_lines: true,
+                replace: {selector:'head',html:'<!-- css -->'}
+            },
+            files: {
+                'tmp/custom_options': ['test/index.html']
+            }
+        }
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'replace_css', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/replace_css.js
@@ -0,0 +1,166 @@
+/*
+ * grunt-replace-css
+ * https://github.com/paipeng/grunt-replace-css
+ *
+ * Copyright (c) 2015 Pai Peng
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+
+    var multilineTrim = function (htmlString) {
+        // split the string into an array by line separator
+        var arr = htmlString.split("\n");
+        // call $.trim on each line
+        arr = arr.map(function (val) {
+            if (val.trim() === "") {
+                return val.trim();
+            } else {
+                return val;
+            }
+        });
+        // filter out the empty lines
+        arr = arr.filter(function (line) {
+                return line != ""
+            }
+        );
+        // join the array of lines back into a string
+        return arr.join("\n");
+    }
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
+    var removeHTMLCode = function (dest, matchArray, replace, remove_blank_lines, $) {
+        grunt.log.subhead('Processing ' + dest.cyan);
+
+        matchArray.forEach(function (option) {
+            //console.log("remove " + JSON.stringify(option));
+            if (!option.selector || !option.attribute || !option.value) {
+                grunt.log.error('remove config missing selector, attribute, and/or value options');
+            } else {
+                $(option.selector).map(function (i, elem) {
+                    if ($(elem).attr(option.attribute) === option.value) {
+                        $(elem).remove();
+                    }
+                });
+            }
+        });
+
+        if (!replace.selector || !replace.html) {
+            grunt.log.error('replace config missing selector, attribute, and/or value options');
+        } else {
+            $(replace.selector).append(replace.html + "\n");
+        }
+
+        var updatedContents = $.html();
+
+        if (remove_blank_lines) {
+            updatedContents = multilineTrim(updatedContents);
+        }
+
+        grunt.file.write(dest, updatedContents);
+        grunt.log.writeln('File ' + (dest ).cyan + ' created/updated.');
+
+    };
+
+    grunt.registerMultiTask('replace_css', 'Replace all linked css files by one', function () {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options({
+            punctuation: '.',
+            separator: ', ',
+            remove_blank_lines: false
+        });
+
+        // Iterate over all specified file groups.
+        this.files.forEach(function (f) {
+            // Concat specified files.
+            var src = f.src.filter(function (filepath) {
+                // Warn on and remove invalid source files (if nonull was set).
+                if (!grunt.file.exists(filepath)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (filepath) {
+                // Read file source.
+                return grunt.file.read(filepath);
+            }).join(grunt.util.normalizelf(options.separator));
+
+            var css_files = [];
+            var htmlparser = require("htmlparser2");
+            var parser = new htmlparser.Parser({
+                onopentag: function (name, attribs) {
+                    grunt.log.writeln("tag " + name);
+                    if (name === "link" && attribs.href !== undefined) {
+                        if (options.prefix !== undefined) {
+                            if (attribs.href.indexOf(options.prefix) == 0) {
+                                //console.log("prefix " + options.prefix);
+
+                                css_files.push(attribs.href);
+                            }
+                        } else {
+                            css_files.push(attribs.href);
+
+                        }
+
+
+                    }
+                },
+                ontext: function (text) {
+                    //console.log("-->", text);
+                },
+                onclosetag: function (tagname) {
+                    if (tagname === "link") {
+                        //console.log("That's it?!");
+                    }
+                },
+                comment: function (text) {
+                    grunt.log.writeln("comment " + text);
+                }
+            });
+
+            parser.write(src);
+            parser.end();
+
+            var css_offset_files = [];
+            var matchArray = [];
+            for (var i in css_files) {
+                matchArray.push({selector: 'link', attribute: 'href', value: css_files[i]});
+                css_offset_files.push(css_files[i].substring(options.offset));
+            }
+            grunt.config.set("css_filenames", css_offset_files);
+            var test = grunt.config.get('css_filenames');
+
+            grunt.log.writeln("test result " + test);
+
+            if (options.replace !== undefined) {
+
+                f.src.filter(function (filepath) {
+                    // Warn on and remove invalid source files (if nonull was set).
+                    if (!grunt.file.exists(filepath)) {
+                        grunt.log.warn('Source file "' + filepath + '" not found.');
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }).map(function (filepath) {
+                    // Read file source.
+                    var src = grunt.file.read(filepath);
+
+                    var cheerio = require('cheerio');
+                    var $ = cheerio.load(src, {lowerCaseAttributeNames: false});
+                    removeHTMLCode(filepath, matchArray, options.replace, options.remove_blank_lines, $);
+
+                });
+
+
+            }
+
+
+            grunt.log.writeln('File "' + f.dest + '" created.');
+        });
+    });
+
+};
